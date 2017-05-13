6b021d1ff1e1a3c52fcda2dcc2a337687562faa0
Peter M. Haverty <phaverty@gene.com>
2017-05-13 10:36:31 -0700

    docstring for GenomeInfo API

--- src/AbstractGenomicVector_type.jl
+++ src/AbstractGenomicVector_type.jl
@@ -1,21 +1,25 @@
-### We will try to provide much of the functionality of the types in this
-### package via the GenomeInfo Interface, the GenoPos Interface and methods
-### for the AbstractGenomicVector type.
+"""
+We will try to provide much of the functionality of the types in this
+package via the GenomeInfo Interface, the GenoPos Interface and methods
+for the AbstractGenomicVector type.
 
-### The GenomeInfo Interface provides
-### access to the genome (name, chromosome lengths, etc.). This Interface requires
-### the following methods:
-### - chr_info
-### which returns a GenomeInfo object, which then provides these methods:
-### - chr_names
-### - chr_lengths
-### - chr_ends
-### - chr_offsets
-### - genome
-### - same_genome
+The GenomeInfo Interface provides
+access to the genome (name, chromosome lengths, etc.). This Interface requires
+the following methods:
+    
+- chr_info
+which returns a GenomeInfo object, which then provides these methods:
 
-### The GenoPos Interface provides access to positional information in the linearized
-### genome or in chromome coordinate (e.g. chr4:1000-1020).
+- chr_names
+- chr_lengths
+- chr_ends
+- chr_offsets
+- genome
+- same_genome
+
+The GenoPos Interface provides access to positional information in the linearized
+genome or in chromome coordinate (e.g. chr4:1000-1020).
+"""
 abstract AbstractGenomicVector{T} <: AbstractVector{T}
 
 for op in [:chr_names, :chr_lengths, :chr_ends, :chr_offsets, :genome]
