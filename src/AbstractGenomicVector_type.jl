658d54122a67edc08178dc966c8756c7251abe19
Peter M. Haverty <phaverty@gene.com>
2017-05-13 12:20:18 -0700

    Moving things around for GenomeInfo Interface

--- src/AbstractGenomicVector_type.jl
+++ src/AbstractGenomicVector_type.jl
@@ -1,38 +1,6 @@
-"""
-We will try to provide much of the functionality of the types in this
-package via the GenomeInfo Interface, the GenoPos Interface and methods
-for the AbstractGenomicVector type.
+##################################
+### AbstractGenomicVector Type ###
+##################################
 
-The GenomeInfo Interface provides
-access to the genome (name, chromosome lengths, etc.). This Interface requires
-the following methods:
-    
-- chr_info
-which returns a GenomeInfo object, which then provides these methods:
-
-- chr_names
-- chr_lengths
-- chr_ends
-- chr_offsets
-- genome
-- same_genome
-
-The GenoPos Interface provides access to positional information in the linearized
-genome or in chromome coordinate (e.g. chr4:1000-1020).
-"""
 abstract AbstractGenomicVector{T} <: AbstractVector{T}
 
-for op in [:chr_names, :chr_lengths, :chr_ends, :chr_offsets, :genome]
-    @eval $(op)(x) = $(op)(chr_info(x))
-end
-
-same_genome(x, y) = chr_info(x) == chr_info(y)
-function same_genome(x, y::Interval)
-    seqname(y) != genome(x) && return false
-    chrs = chromosomes( [leftposition(y),rightposition(y)], chr_info(x) )
-    chrs[1] != chrs[2] && return false
-    return true
-end
-same_genome(x::Interval, y) = same_genome(y,x)
-
-@doc (@doc AbstractGenomicVector) GenomeInfo, chr_info, chr_names, chr_lengths, chr_ends, chr_offsets, genome, same_genome
--- src/GenomeInfo_type.jl
+++ src/GenomeInfo_type.jl
@@ -1,6 +1,6 @@
-###################
-### GenomeInfo Type
-###################
+#######################
+### GenomeInfo Type ###
+#######################
 
 """
 # GenomeInfo Type
@@ -17,6 +17,26 @@ chr_lengths(chrinfo)
 chr_ends(chrinfo)
 chr_offsets(chrinfo)
 chrinfo[2] # 5e5
+
+## GenomeInfo Interface
+Much of the functionality of the types in the GenomicVectors.jl
+package via the GenomeInfo Interface, which provides
+access to the genome (name, chromosome lengths, etc.). This Interface requires
+the following methods:
+    
+- chr_info
+which returns a GenomeInfo object, which then provides these methods:
+
+- chr_names
+- chr_lengths
+- chr_ends
+- chr_offsets
+- genome
+- same_genome
+
+The GenoPos Interface provides access to positional information in the linearized
+genome or in chromosome coordinate (e.g. chr4:1000-1020).
+
 ```
 """
 immutable GenomeInfo{T1<:Integer}
@@ -65,4 +85,18 @@ end
 
 Base.getindex(x::GenomeInfo, i::Union{String, Integer}) = x.chr_ends[i]
 
-@doc (@doc GenomeInfo) chr_names, chr_lengths, chr_ends, chr_offsets, genome
+## GenomeInfo Interface
+for op in [:chr_names, :chr_lengths, :chr_ends, :chr_offsets, :genome]
+    @eval $(op)(x) = $(op)(chr_info(x))
+end
+
+same_genome(x, y) = chr_info(x) == chr_info(y)
+function same_genome(x, y::Interval)
+    seqname(y) != genome(x) && return false
+    chrs = chromosomes( [leftposition(y),rightposition(y)], chr_info(x) )
+    chrs[1] != chrs[2] && return false
+    return true
+end
+same_genome(x::Interval, y) = same_genome(y,x)
+
+@doc (@doc AbstractGenomicVector), chr_info, chr_names, chr_lengths, chr_ends, chr_offsets, genome, same_genome
