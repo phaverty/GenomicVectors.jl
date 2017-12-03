f5105b16a53092059eacf2b2386250a3e3ec36ed
Peter M. Haverty <phaverty@gene.com>
2017-12-03 12:13:40 -0800

    basic disjoin(gr), needs more testing

--- src/GenomicVectors.jl
+++ src/GenomicVectors.jl
@@ -27,7 +27,8 @@ import GenomicFeatures: coverage
 export genome, chr_info, chromosomes, chrpos, chrindex, genopos, slide!, slide
 export strands, nearest, genostarts, genoends, starts, ends, widths
 export _genostarts, _genoends, _strands
-export findoverlaps, eachrange, remove_overlaps, select_overlaps, disjoin, gaps, coverage
+export findoverlaps, eachrange, remove_overlaps, select_overlaps
+export disjoin, gaps, coverage, collapse
 
 # Delegations
 import Base: similar, copy, unique, size, length, endof, issubset, vcat, union, intersect, setdiff, symdiff, append!, prepend!, resize!
