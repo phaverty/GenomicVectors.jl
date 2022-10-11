b8925f85d1f1e542c2951e3abfda0e481c8e72b8
Peter Haverty <peterhaverty@alumni.ucdavis.edu>
2022-10-10 22:01:03 -0700

    get docs working

--- docs/make.jl
+++ docs/make.jl
@@ -1,12 +1,9 @@
 using Documenter, GenomicVectors
 
 makedocs(
-         modules = [GenomicVectors],
-         format = :html,
          sitename = "GenomicVectors"
          )
 
 deploydocs(
-           repo = "github.com/phaverty/GenomicVectors.jl.git",
-           julia = "release"
+           repo = "github.com/phaverty/GenomicVectors.jl.git"
            )
